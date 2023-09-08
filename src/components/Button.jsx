b60e6cdf8eebedd60a66634a978a1105b862a497
Tahzibi <m.r.tahzibi@gmail.com>
2023-09-08 23:05:42 +0330

    Add section Super quality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,9 @@ const Button = ({lable, iconURL}) => {
   return (
     <button className="flex justify-center items-center gap-2  text-lg text-white bg-coral-red border-coral-red py-4 px-10 rounded-full font-montserrat">
         {lable}
-        <img src={iconURL} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
+        {iconURL && (
+          <img src={iconURL} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
+        )}
     </button>
   )
 }
--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,5 +1,34 @@
+import Button from "./Button";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="max-container max-sm:mt-12 flex flex-col lg:flex-row gap-10 justify-between items-center">
+      <div className="flex flex-col justify-start items-start lg:w-1/2">
+        <h1 className="text-4xl font-palanquin font-bold ">
+          We Provide You <span className="text-coral-red">Super Quality </span>{" "}
+          Shoes
+        </h1>
+        <p className="text-slate-gray my-6 text-xl leading-8">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+
+        <p className="mt-6 info-text lg:max-w-lg">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-10">
+          <Button lable="View details" />
+        </div>
+      </div>
+      <div className="flex justify-center items-center flex-1">
+        <img src={shoe8} alt="product detail" width={570}  height={522} className="object-contain" />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
