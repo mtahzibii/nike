06cd60c7ddcd7ee0382129647d4d0caa0e1c0b57
Tahzibi <m.r.tahzibi@gmail.com>
2023-09-14 18:11:48 +0330

    Add section subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = ({lable, iconURL, backgroundColor, borderColor, textColor}) => {
   return (
-    <button className={`flex justify-center items-center gap-2  text-lg text-white ${backgroundColor ? backgroundColor : 'bg-coral-red'}  ${borderColor ?  'borderColor' : 'border-coral-red'} ${textColor ? textColor : 'text-white'} py-4 px-10 rounded-full font-montserrat border`}>
+    <button className={`flex justify-center items-center gap-2 text-lg text-white ${backgroundColor ? backgroundColor : 'bg-coral-red'}  ${borderColor ?  'borderColor' : 'border-coral-red'} ${textColor ? textColor : 'text-white'} py-4 px-10 rounded-full font-montserrat border`}>
         {lable}
         {iconURL && (
           <img src={iconURL} alt="icon" className="ml-2 w-5 h-5 rounded-full" />
--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,7 +1,24 @@
+import Button from "./Button";
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section id="contact-us">
+      <div className="max-container flex max-lg:flex-col justify-between items-center gap-8">
+        <h3 className="text-4xl font-bold font-palanquin mr-20 leading-[68px] lg:max-w-md">
+          Sign Up for <span className="text-coral-red">Updates</span> &
+          Newsletter
+        </h3>
+        <div className="lg:max-w-[40%] w-full flex flex-col justify-between sm:flex-row lg:justify-between border border-slate-700 rounded-full px-4 py-2 pl-6 relative ml-10 text-slate-gray max-sm:gap-8 max-sm:pl-0 max-sm:border-none">
+          <input
+            type="text"
+            placeholder="subscribe@nike.com"
+            className="outline-none max-sm:border border-slate-gray rounded-full p-4"
+          />
+          <Button lable="Sign Up" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;
